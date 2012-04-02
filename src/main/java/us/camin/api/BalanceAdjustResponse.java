9c9355e7f8dc48240c42877dec6ad1aa75dd9fb3
Trever Fischer <tdfischer@fedoraproject.org>
2012-04-02 15:45:39 -0400

    Add missing file

---
+++ src/main/java/us/camin/api/BalanceAdjustResponse.java
@@ -0,0 +1,25 @@
+package us.camin.api;
+
+/*
+    This file is part of Caminus
+
+    Caminus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Caminus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Caminus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+public class BalanceAdjustResponse {
+    public boolean success = false;
+    public double newBalance = 0;
+    public String message = "";
+}
