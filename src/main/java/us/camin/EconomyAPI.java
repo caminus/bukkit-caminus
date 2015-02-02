83e9e4a1a3b5749b63c694af4e7a65e7042cac2b
Torrie Fischer <tdfischer@hackerbots.net>
2015-02-01 17:10:29 -0800

    Fix balance adjustments

--- src/main/java/us/camin/EconomyAPI.java
+++ src/main/java/us/camin/EconomyAPI.java
@@ -84,7 +84,7 @@ public class EconomyAPI implements Economy {
     private EconomyResponse adjustPlayer(String playerName, double amount) {
         BalanceAdjustResponse resp;
         try {
-            resp = m_p.api().adjustBalance(playerName, -amount);
+            resp = m_p.api().adjustBalance(playerName, amount);
         } catch (IOException e) {
             return new EconomyResponse(0, 0, ResponseType.FAILURE, "Could not contact api.camin.us.");
         }
@@ -97,7 +97,7 @@ public class EconomyAPI implements Economy {
 
     @Override
     public EconomyResponse withdrawPlayer(String playerName, double amount) {
-        return adjustPlayer(playerName, amount);
+        return adjustPlayer(playerName, -amount);
     }
 
     @Override
